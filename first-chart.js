1d02759a74e687228c2d7b77744af8dbd1d35dce
Anna J <ajeszeck@gmail.com>
2017-06-15 21:17:52 -0700

    adds functionality to add votes to the tallies

--- first-chart.js
+++ first-chart.js
@@ -7,6 +7,31 @@ google.charts.setOnLoadCallback(drawChart);
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
+
+var toppingCounter = {
+  tomato: 6,
+  pepperoni: 7,
+  peppers: 4,
+  pineapple: 6,
+  shrimp: 1
+}
+
+window.onload = function() {
+  document.getElementById("tomato").onclick = vote;
+  document.getElementById("pepperoni").onclick = vote;
+  document.getElementById("peppers").onclick = vote;
+  document.getElementById("pineapple").onclick = vote;
+  document.getElementById("shrimp").onclick = vote;
+
+}
+
+function vote() {
+  console.log(this.id); //accept even though we don't understand it.
+  toppingCounter[this.id] = toppingCounter[this.id] + 1;
+  drawChart();
+  drawChartTwo();
+}
+
 function drawChart() {
 
   // Create the data table.
@@ -15,11 +40,11 @@ function drawChart() {
   data.addColumn('number', 'Number of Fans'); //adds column with a number for slice.
   data.addRows([//each item in array lists a topping and a slice number.
     //each row should have the same number of elements as columns you have added.
-    ['Tomato', 6],
-    ['Pepperoni', 7],
-    ['Peppers', 4],
-    ['Pineapple', 6],
-    ['Fried Shrimp', 1]
+    ['Tomato', toppingCounter.tomato],
+    ['Pepperoni', toppingCounter.pepperoni],
+    ['Peppers', toppingCounter.peppers],
+    ['Pineapple', toppingCounter.pineapple],
+    ['Shrimp', toppingCounter.shrimp]
   ]);
 
   // Set chart options
@@ -33,32 +58,33 @@ function drawChart() {
 }
 
 google.charts.load("current", {packages:['corechart']});
-    google.charts.setOnLoadCallback(drawChartTwo);
-    function drawChartTwo() {
-      var data = google.visualization.arrayToDataTable([
-        ["Topping", "Popularity", { role: "style" } ],
-        ["Tomato", 6, "blue"],
-        ["Pepperoni", 7, "red"],
-        ["Peppers", 4, "orange"],
-        ["Pineapple", 6, "green"],
-        ["Fried Shrimp", 1, "purple"]
-      ]);
+google.charts.setOnLoadCallback(drawChartTwo);
 
-      var view = new google.visualization.DataView(data);
-      view.setColumns([0, 1,
-                       { calc: "stringify",
-                         sourceColumn: 1,
-                         type: "string",
-                         role: "annotation" },
-                       2]);
+function drawChartTwo() {
+  var data = google.visualization.arrayToDataTable([
+    ["Topping", "Popularity", { role: "style" } ],
+    ["Tomato", toppingCounter.tomato, "blue"],
+    ["Pepperoni", toppingCounter.pepperoni, "red"],
+    ["Peppers", toppingCounter.peppers, "orange"],
+    ["Pineapple", toppingCounter.pineapple, "green"],
+    ["Shrimp", toppingCounter.shrimp, "purple"]
+  ]);
 
-      var options = {
-        title: "Favorite Toppings in the HTML 300 Class",
-        width: 600,
-        height: 400,
-        bar: {groupWidth: "95%"},
-        legend: { position: "none" },
-      };
-      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
-      chart.draw(view, options);
-  }
+  var view = new google.visualization.DataView(data);
+  view.setColumns([0, 1,
+                   { calc: "stringify",
+                     sourceColumn: 1,
+                     type: "string",
+                     role: "annotation" },
+                   2]);
+
+  var options = {
+    title: "Favorite Toppings in the HTML 300 Class",
+    width: 600,
+    height: 400,
+    bar: {groupWidth: "95%"},
+    legend: { position: "none" },
+  };
+  var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+  chart.draw(view, options);
+}
